ffd51df5f46f16ac183e090e4d794560a6c6e07e
Elizabeth <rxxivkey@gmail.com>
2025-06-02 15:52:39 +0300

    wrote js

---
+++ work.js
@@ -0,0 +1,42 @@
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+let year = 2007;
+alert(year);
+
+let nameCreator = 'Brendan';
+alert(nameCreator);
+
+a = 10;
+b = 2;
+alert(a + b);
+alert(a - b);
+alert(a * b);
+alert(a / b);
+
+result = b**5;
+alert(result);
+
+a = 9;
+b =  2;
+alert(a % b);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num ++;
+num --;
+alert(num);
+
+let userName = prompt("Как вас зовут?");
+alert(`Привет, ${userName}!`);
+
+const user = {
+    name: Печенька,
+    age: 3,
+    isAdmin: false
+};
