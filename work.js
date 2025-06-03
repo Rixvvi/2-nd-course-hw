5b794117a4cc5922715206f79d38c9a6f4ec040d
Elizabeth <rxxivkey@gmail.com>
2025-06-03 16:30:52 +0300

    new homework 3

--- work.js
+++ work.js
@@ -40,3 +40,77 @@ const user = {
     age: 3,
     isAdmin: false
 };
+
+let password = '12345';
+let value = prompt('Введите пароль');
+if (value === password) {
+    console.log("Пароль введен верно")
+} else {
+    console.log("Пароль введен неправильно")
+}
+
+let c = 7;
+if (c > 0 && c < 10) {
+    console.log("Верно")
+} else {
+    console.log("Неверно")
+}
+c = 0;
+if (c > 0 && c < 10) {
+    console.log("Верно")
+} else {
+    console.log("Неверно")
+}
+c = 10;
+if (c > 0 && c < 10) {
+    console.log("Верно")
+} else {
+    console.log("Неверно")
+}
+c = -3;
+if (c > 0 && c < 10) {
+    console.log("Верно")
+} else {
+    console.log("Неверно")
+}
+c = 2
+if (c > 0 && c < 10) {
+    console.log("Верно")
+} else {
+    console.log("Неверно")
+}
+
+let d = 101;
+let e = 16;
+if (d > 100 || e > 100) {
+    console.log("Верно")
+} else {
+    console.log("Неверно")
+}
+
+let aa = '2';
+let bb = '3';
+// Код выше изменять менять нельзя. Чтобы решить задачу исправьте код ниже:
+aa = 2;
+bb = 3;
+alert(aa + bb);
+
+
+let monthNumber = 12;
+
+switch (monthNumber) {
+   case monthNumber === 12 || monthNumber === 1 || monthNumber === 2:
+      console.log("зима");
+      break;
+   case monthNumber === 3 || monthNumber === 4 || monthNumber === 5:
+      console.log("весна");
+      break;
+    case monthNumber === 6 || monthNumber === 7 || monthNumber === 8:
+        console.log("лето");
+        break;
+    case monthNumber === 9 || monthNumber === 10 || monthNumber === 11:
+        console.log("осень");
+        break;
+   default:
+      console.log('Месяцев всего 12');
+}
